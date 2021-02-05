0ebec56d757badca01e3ea67a5b5d9707a2dae39
Julia Top <votyakovajulia@gmail.com>
2021-02-05 20:05:42 +0300

    Day2.Task1

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,26 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        int floorCount = Integer.parseInt(string);
+
+        if (floorCount > 0 && floorCount < 5){
+            System.out.println("Малоэтажный дом");
+        }
+        else if (floorCount >= 5 && floorCount <=8) {
+            System.out.println("Среднеэтажный дом");
+        }
+        else if (floorCount >= 9) {
+            System.out.println("Многоэтажный дом");
+        }
+        else {
+            System.out.println("Ошибка ввода");
+        }
+
 
     }
 }
