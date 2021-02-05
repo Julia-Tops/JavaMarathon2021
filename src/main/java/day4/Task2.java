0f5e3de26c90055956797811a92e67813b6471a1
Julia Top <votyakovajulia@gmail.com>
2021-02-05 23:08:45 +0300

    Day4.Task2

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,45 @@
 package day4;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        int[] array = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10000);
+        }
+
+
+        int max = 0;
+        for (int num : array
+        ) {
+           if(num > max){
+               max = num;
+           }
+        }
+        System.out.println(max);
+
+        int min = 10000;
+        for (int num : array
+             ) {
+            if ( num < min) {
+                min = num;
+            }
+        }
+        System.out.println(min);
+
+        int sumElementsEndZero = 0;
+        int countElementsEndZero = 0;
+        for (int num : array
+             ) {
+            if(num%10 == 0) {
+                countElementsEndZero++;
+                sumElementsEndZero += num;
+            }
+        }
+        System.out.println(countElementsEndZero);
+        System.out.println(sumElementsEndZero);
 
     }
 }
