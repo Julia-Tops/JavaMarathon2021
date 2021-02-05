0b0d16c0e85d33aab3fc38fe4dd9516cafecb60c
Julia Top <votyakovajulia@gmail.com>
2021-02-05 23:32:44 +0300

    Day4.Task3

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,33 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        int[][] matrix = new int[8][12];
+        Random random= new Random();
+
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(50);
+            }
+        }
+        for (int[] row : matrix)
+            System.out.println(Arrays.toString(row));
+        int maxSumStr = 0;
+        int indexMaxStr = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            int sumStr = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                sumStr += matrix[i][j];
+            }
+            if(sumStr >= maxSumStr){
+                maxSumStr = sumStr;
+                indexMaxStr = i;
+            }
+        }
+        System.out.println(indexMaxStr);
 
     }
 }
