b833aba9d1bae7cdd784a8a7941c959a57edeec0
Julia Top <votyakovajulia@gmail.com>
2021-02-05 21:43:05 +0300

    Day3.Task2

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,15 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        double a = new Scanner(System.in).nextDouble();
+        double b = new Scanner(System.in).nextDouble();
+        while (b != 0){
+            System.out.println(a/b);
+           a = new Scanner(System.in).nextDouble();
+           b = new Scanner(System.in).nextDouble();
+        }
     }
 }
