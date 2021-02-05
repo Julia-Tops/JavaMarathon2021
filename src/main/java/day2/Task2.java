05d4b15ea6f3f4c139c685e85ce827e9f0eb5f7e
Julia Top <votyakovajulia@gmail.com>
2021-02-05 20:19:03 +0300

    Day2.Task2

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
 
+        int a = Integer.parseInt(scanner1.nextLine());
+        int b = Integer.parseInt(scanner2.nextLine());
+        if (a >= b){
+            System.out.println("Некорректный ввод");
+        }
+        else {
+            for (int i = a + 1; i < b; i++) {
+                if(i%5 == 0 && i%10 != 0){
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
 }
