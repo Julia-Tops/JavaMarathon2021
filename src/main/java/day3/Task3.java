86f934707e21978060c9aa6740bb87a3a408d465
Julia Top <votyakovajulia@gmail.com>
2021-02-05 22:03:52 +0300

    Day3.Task3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
 
+        while (count < 5) {
+            Double a = scanner.nextDouble();
+            Double b = scanner.nextDouble();
+            if(b == 0)
+            {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            count++;
+            System.out.println(a/b);
+        }
     }
 }
