c13058344e5b6e98b99af4ff25014a8aafad317b
Julia Top <votyakovajulia@gmail.com>
2021-02-05 23:41:37 +0300

    Day4.Task4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,26 @@
 package day4;
 
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
+        int[] array = new int[100];
+        Random random = new Random();
 
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = random.nextInt(10000);
+        }
+
+        int maxSumOfThreeElements = 0;
+        int indexOfMaxSum = 0;
+        for (int i = 0; i < array.length-2; i++) {
+         int sumOfThreeElements = array[i] + array[i + 1] + array[i + 2];
+         if (sumOfThreeElements > maxSumOfThreeElements) {
+             maxSumOfThreeElements = sumOfThreeElements;
+             indexOfMaxSum = i;
+         }
+        }
+        System.out.println(maxSumOfThreeElements);
+        System.out.println(indexOfMaxSum);
     }
 }
