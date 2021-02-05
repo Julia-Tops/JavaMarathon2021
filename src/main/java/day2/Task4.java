7f4cba51965d91121a4ff92c87fd349599ada823
Julia Top <votyakovajulia@gmail.com>
2021-02-05 20:43:35 +0300

    Day2.Task3

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        double x = new Scanner(System.in).nextDouble();
+        if(x >= 5) {
+            double y = (x*x - 10)/(x + 7);
+            System.out.print(y);
+        }
+        else if(x > -3 && x < 5) {
+            double y = (x + 30)*(x*x - 2);
+            System.out.print(y);
+        }
+        else {
+            double y = 420;
+        }
     }
 }
