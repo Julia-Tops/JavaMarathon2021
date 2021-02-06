f9c0b6b916456a7a2f74f0e52af33ba0aeb109d6
Julia Top <votyakovajulia@gmail.com>
2021-02-06 13:55:00 +0300

    Day6.Task2

--- src/main/java/day6/Task2.java
+++ src/main/java/day6/Task2.java
@@ -2,6 +2,11 @@ package day6;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Airbus", 2020, 150, 50);
+        airplane.setLength(150031);
+        airplane.setYear(2021);
+        airplane.fillUp(15000);
+        airplane.info();
 
     }
 }
