fae4e4bbe6233179b085ca96398d0139d859a3c6
Julia Top <votyakovajulia@gmail.com>
2021-02-06 11:49:22 +0300

    Day5.Task1

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,46 @@
 package day5;
 
+class Car{
+    int year;
+    String color;
+    String model;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
+
 public class Task1 {
+
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setYear(2020);
+        car.setColor("black");
+        car.setModel("Audi");
+
+        System.out.println(car.getYear());
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
 
     }
 }
