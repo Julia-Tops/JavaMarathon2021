f500553d9fecd586138e6145a46ab82e219bd88a
Julia Top <votyakovajulia@gmail.com>
2021-02-06 18:09:24 +0300

    Day8.Task1,2 -прошли тест

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,11 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 20001; i++) {
+            stringBuilder.append(i + " ");
+        }
+        System.out.println(stringBuilder.toString());
 
     }
 }
