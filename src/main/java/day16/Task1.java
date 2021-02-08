db012953547add162e54dc240b0288a62171eda1
Julia Top <votyakovajulia@gmail.com>
2021-02-08 15:48:41 +0300

    Day16.Task 1 - Прошла тест

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,37 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("test1");
+        printResult(file);
+    }
+
+    public static void printResult(File file){
+        try {
+            Scanner scanner = new Scanner(file);
+            String[] strings = scanner.nextLine().split(" ");
+            int[] numbers = new int[strings.length];
+            for (int i = 0; i < strings.length; i++) {
+                numbers[i] = Integer.parseInt(strings[i]);
+            }
+            int sum = 0;
+            for (int numb : numbers
+                 ) {
+                sum += numb;
+            }
+            double average =  sum/(double) numbers.length;
+
+            System.out.printf(average + " --> %.3f", average);
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
 
     }
 }
