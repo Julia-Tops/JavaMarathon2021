3368f9269b23cf84069f9aa295be21a6e35b4df7
Julia Top <votyakovajulia@gmail.com>
2021-02-09 15:00:49 +0300

    Day18.Task 2 решено (без теста)

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,29 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        int count = 717771277;
 
+        System.out.println(count7(count));
+
+    }
+    public  static int count7(int count){
+        if (count > 10) {
+            if (count%10 == 7)
+            {
+                return 1 + count7(count/10);
+            }
+            else {
+                return count7(count/10);
+            }
+        }
+        else {
+            if (count%10 == 7)
+            {
+                return 1 ;
+            }
+            else {
+                return 0;
+            }
+        }
     }
 }
