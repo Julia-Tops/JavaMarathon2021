2fff6f3a7211ce6e25b039bc5c9282621013d938
Julia Top <votyakovajulia@gmail.com>
2021-02-09 13:39:35 +0300

    Day18.Task 1 решено (без теста)

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,20 @@
 package day18;
 
+import day17.ChessPiece;
+
 public class Task1 {
     public static void main(String[] args) {
+        int[] numbers = {1, 10, 1241,50402,-50,249,10215,665,2295,7,311};
+    System.out.println(recursionSum(numbers, numbers.length -1));
+    }
+
+    private static int  recursionSum(int[] numbers, int n ) {
+        if(n == 0){
+            return numbers[n];
+        }
+        return numbers[n] + recursionSum(numbers, n -1);
+
+
 
     }
 }
